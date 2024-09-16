f9fa5966dbda8e759bbfd5493902781b76ba39c0
Gitlab CI Runner <gitlab-ci-runner@bravantic.com>
2024-09-17 00:28:13 +0100

    Improving messages

--- remove_old_kernels.sh
+++ remove_old_kernels.sh
@@ -1,4 +1,4 @@
- #!/bin/bash -e
+#!/bin/bash -e
 # Run this script without any arguments for a dry run
 # Run the script with root and with exec arguments for removing old kernels and modules after checking
 # the list printed in the dry run
@@ -6,6 +6,7 @@
 uname -a
 IN_USE=$(uname -a | awk '{ print $3 }')
 echo "Your in use kernel is $IN_USE"
+echo ""
 
 OLD_KERNELS=$(
     dpkg --get-selections |
@@ -16,22 +17,43 @@ OLD_KERNELS=$(
         grep -Ei 'linux-image|linux-headers|linux-modules' |
         awk '{ print $1 }'
 )
-echo "Old Kernels to be removed:"
-echo "$OLD_KERNELS"
+
+if [[ -z "$OLD_KERNELS" ]]; then
+  echo "No old kernels found."
+else
+  echo "Old Kernels to be removed:"
+  echo "$OLD_KERNELS"
+fi
+
+echo ""
 
 OLD_MODULES=$(
     ls /lib/modules |
     grep -v "${IN_USE%%-generic}" |
     grep -v "${IN_USE}"
 )
-echo "Old Modules to be removed:"
-echo "$OLD_MODULES"
 
-if [ "$1" == "exec" ]; then
-  apt-get purge $OLD_KERNELS
-  for module in $OLD_MODULES ; do
-    rm -rf /lib/modules/$module/
-  done
+if [[ -z "$OLD_MODULES" ]]; then
+  echo "No old modules found."
 else
-    echo "If all looks good, run it again like this: sudo remove_old_kernels.sh exec"
+  echo "Old Modules to be removed:"
+  echo "$OLD_MODULES"
+fi
+
+echo ""
+echo ""
+
+# Combine check and message for clarity
+if [[ -n "$OLD_KERNELS" || -n "$OLD_MODULES" ]]; then
+  if [ "$1" == "exec" ]; then
+    apt-get purge $OLD_KERNELS
+    for module in $OLD_MODULES ; do
+      rm -rf /lib/modules/$module/
+    done
+  else
+    echo "If all looks good, run it again like this: sudo   
+ remove_old_kernels.sh exec"
+  fi
+else
+  echo "Nothing found to delete."
 fi
